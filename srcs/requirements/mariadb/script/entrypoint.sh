f19b49968ac20438ee0afbc3fd167e7c221ba80f
Suliac <122944617+Sul-iac@users.noreply.github.com>
2025-03-25 00:17:29 +0100

    Dockerfile wordpress

--- srcs/requirements/mariadb/script/entrypoint.sh
+++ srcs/requirements/mariadb/script/entrypoint.sh
@@ -5,14 +5,14 @@ set -e
 if [! -d "/var/lib/mysql/mysql"]; then
 	echo "MariaDb initialisation..."
 	mysqld --initialize-insecure
-	if [! -f /docker-entrypoint-initdb.d/init.sql]; then
+	if [! -f /docker-entrypoint-initdb.d/init.sql]; then # if this file does not exist
 		echo "Executing initialization script..."
-		envsubst < /docker-entrypoint-initdb.d/init.sql > /tmp/init.sql
-		mysqld --skip-networking &
-		pid="$!"
+		envsubst < /docker-entrypoint-initdb.d/init.sql > /tmp/init.sql # replace initial env with init.sql
+		mysqld --skip-networking &	# execute init command in bg w/o password by default - create files and folders
+		pid="$!"					# get the mariadb pid running in background set above
 		sleep 5
-		mysql < /tmp/init.sql
-		kill "$pid"
+		mysql < /tmp/init.sql		# execute init.sql commands and sets everything in the db
+		kill "$pid"					# kill the previous processus when init done
 	fi
 fi
 echo "Starting  mysql..."
