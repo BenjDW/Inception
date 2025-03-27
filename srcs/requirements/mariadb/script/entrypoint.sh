b55cab1b9c269bae4cf62156f6ada7c078fa333e
BenjDW <dewitskibenji@live.fr>
2025-03-27 08:07:51 -0700

    user created, all probleme solve on mariadb for the moment, script broken is now functionnal

--- srcs/requirements/mariadb/script/entrypoint.sh
+++ srcs/requirements/mariadb/script/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
+# sleep 20
+# if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "====> MariaDB initialization..."
+	# mkdir -p /var/run/mysqld && chown -R mysql:mysql /var/run/mysqld \
+    # && chown -R mysql:mysql /var/lib/mysql
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db
     # Ensuite, si init.sql existe, on l'exécute.
     if [ -f /docker-entrypoint-initdb.d/init.sql ]; then
@@ -10,12 +13,12 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
         # On lance mariadbd en background sans contrainte de password
         mariadbd --skip-networking --user=mysql &
         pid="$!"
-        sleep 30
+        sleep 5
         # On exécute le script SQL
         mysql --user=root --password=rootpassword < /docker-entrypoint-initdb.d/init.sql
         kill "$pid"
     fi
-fi
+# fi
 
 echo "====> Starting mariadb..."
 exec mariadbd --user=mysql
