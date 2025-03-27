f866cfa8fb93796708e99e52511e04e4a7c09c1a
BenjDW <dewitskibenji@live.fr>
2025-03-27 12:42:23 -0700

    problem solve, wp dockerfile in progress , all seems ok

---
+++ srcs/requirements/wordpress/script/auto_config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sleep 10
+
+wp config create --allow-root \
+	--dbname=$SQL_DATABASE \
+	--dbuser=$SQL_USER \
+	--dbpass=$SQL_PASSWORD \
+	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+wp core install --url='https://bde-wits42.fr' --title='Blog Title' --admin_user='master' --admin_password='password' --admin_email='email@domain.com'
+
+# wp user create
