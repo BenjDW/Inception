499f754c034d8f84511eb0405bd2575a3a12afc0
BenjDW <dewitskibenji@live.fr>
2025-03-29 11:58:44 -0700

    fix all probleme in all files, wordpress and nginx is working now

--- srcs/requirements/wordpress/script/auto_config.sh
+++ srcs/requirements/wordpress/script/auto_config.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
 
-sleep 10
+# Attendre que la DB soit prête
+until mysqladmin ping -h"mariadb" --silent; do
+    echo "Waiting for MariaDB..."
+    sleep 2
+done
 
-wp config create --allow-root \
-	--dbname=$SQL_DATABASE \
-	--dbuser=$SQL_USER \
-	--dbpass=$SQL_PASSWORD \
-	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+cd /var/www/wordpress
 
-wp core install --allow-root --url='https://bde-wits42.fr' --title='Blog Title' --admin_user='master' --admin_password='password' --admin_email='email@domain.com'
+if [ -f wp-config.php ]; then
+    echo "WordPress is already set up."
+else
+    echo "No wp-config.php found. Setting up WordPress..."
 
-# wp user create
+    wp config create --allow-root \
+        --dbname="${SQL_DATABASE}" \
+        --dbuser="${SQL_USER}" \
+        --dbpass="${SQL_PASSWORD}" \
+        --dbhost="mariadb:3306" \
+        --path="/var/www/wordpress"
+
+    if ! wp core is-installed --allow-root; then
+        wp core install --allow-root \
+            --url="localhost" \
+            --title="Blog Title" \
+            --admin_user="master" \
+            --admin_password="password" \
+            --admin_email="email@domain.com" \
+            --path="/var/www/wordpress"
+    fi
+fi
+
+# Lancer PHP-FPM par le symlink
+echo "Starting php-fpm..."
+exec php-fpm -F
