f542d56c426a990e7fe821902ec0aed3193f426a
BenjDW <dewitskibenji@live.fr>
2025-04-01 13:10:56 -0700

    change brut value to  value, is seem functionnal too

--- srcs/requirements/wordpress/script/auto_config.sh
+++ srcs/requirements/wordpress/script/auto_config.sh
@@ -18,7 +18,7 @@ else
         --dbuser="${SQL_USER}" \
         --dbpass="${SQL_PASSWORD}" \
         --dbhost="mariadb:3306" \
-        --path="/var/www/html"
+        --path="$WP_PATH"
     if ! grep -q "define('WP_HOME'" "/var/www/html/wp-config.php"; then
         echo "Ajout des constantes WP_HOME et WP_SITEURL au début de wp-config.php..."
         # Insérer les définitions WP_HOME et WP_SITEURL après "<?php"
@@ -29,14 +29,14 @@ else
 
     # if ! wp core is-installed --allow-root; then
         wp core install --allow-root \
-            --url="https://bde-wits.42.fr" \
-            --title="Blog Title" \
-            --admin_user="master" \
-            --admin_password="password" \
-            --admin_email="email@domain.com" \
-            --path="/var/www/html"
+            --url="$WP_DOMAIN" \
+            --title="$Wp_TITLE" \
+            --admin_user="$WP_ADMIN_USER" \
+            --admin_password="$WP_ADMIN_MDP" \
+            --admin_email="$WP_ADMIN_MAIL" \
+            --path="$WP_PATH"
 # cree le second user a test
-		wp user create "simple_user" "basic@example.com" --role=editor --user_pass="simple" --allow-root --path="/var/www/html"
+		wp user create "$WP_BASIC_USER" "$WP_USER_MAIL" --role=editor --user_pass="$WP_USER_MDP" --allow-root --path="$WP_PATH"
     # fi
 fi
 
