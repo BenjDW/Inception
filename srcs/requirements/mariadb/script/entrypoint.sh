e144f7430e68e87e7d314abdc0af00e82807aabe
BenjDW <dewitskibenji@live.fr>
2025-04-02 07:25:43 -0700

    remove all comments and unused line

--- srcs/requirements/mariadb/script/entrypoint.sh
+++ srcs/requirements/mariadb/script/entrypoint.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 set -e
 
-# sleep 20
-# if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "====> MariaDB initialization..."
-	# mkdir -p /var/run/mysqld && chown -R mysql:mysql /var/run/mysqld \
-    # && chown -R mysql:mysql /var/lib/mysql
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db
-    # Ensuite, si init.sql existe, on l'exécute.
     if [ -f /docker-entrypoint-initdb.d/init.sql ]; then
         echo "====> Running custom init.sql..."
 		envsubst < /tmp/init.sql > /docker-entrypoint-initdb.d/init.sql
-        # On lance mariadbd en background sans contrainte de password
         mariadbd --skip-networking --user=mysql &
         pid="$!"
         sleep 5
-        # On exécute le script SQL
         mysql --user=root --password=rootpassword < /docker-entrypoint-initdb.d/init.sql
         kill "$pid"
     fi
-# fi
 
 echo "====> Starting mariadb..."
 exec mariadbd --user=mysql
--- srcs/requirements/wordpress/script/auto_config.sh
+++ srcs/requirements/wordpress/script/auto_config.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Attendre que la DB soit prête
 until mysqladmin ping -h"mariadb" --silent; do
     echo "Waiting for MariaDB..."
     sleep 2
@@ -21,13 +20,10 @@ else
         --path="$WP_PATH"
     if ! grep -q "define('WP_HOME'" "/var/www/html/wp-config.php"; then
         echo "Ajout des constantes WP_HOME et WP_SITEURL au début de wp-config.php..."
-        # Insérer les définitions WP_HOME et WP_SITEURL après "<?php"
         sed -i "1a\define('WP_HOME', 'https://bde-wits.42.fr');\ndefine('WP_SITEURL', 'https://bde-wits.42.fr');\n" "/var/www/html/wp-config.php"
     else
         echo "Les constantes WP_HOME et WP_SITEURL sont déjà définies."
     fi
-
-    # if ! wp core is-installed --allow-root; then
         wp core install --allow-root \
             --url="$WP_DOMAIN" \
             --title="$Wp_TITLE" \
@@ -35,11 +31,8 @@ else
             --admin_password="$WP_ADMIN_MDP" \
             --admin_email="$WP_ADMIN_MAIL" \
             --path="$WP_PATH"
-# cree le second user a test
 		wp user create "$WP_BASIC_USER" "$WP_USER_MAIL" --role=editor --user_pass="$WP_USER_MDP" --allow-root --path="$WP_PATH"
-    # fi
 fi
 
-# Lancer PHP-FPM par le symlink
 echo "Starting php-fpm..."
 exec php-fpm -F
